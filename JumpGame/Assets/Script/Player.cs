3ccff30593b4bfb4048e13afb151c16c1cf66c32
KuongGB <cuong3right@gmail.com>
2024-03-04 13:51:17 +0700

    update cuong scenes and menu

--- JumpGame/Assets/Script/Player.cs
+++ JumpGame/Assets/Script/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
+
 using UnityEngine;
 
 public class Player : MonoBehaviour
