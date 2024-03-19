7126c86cf109ca46bbc6f43b352f0885144cbe7b
KuongGB <cuong3right@gmail.com>
2024-03-19 16:17:21 +0700

    last commit

--- JumpGame/Assets/Cuong/Assets/Character/PlayerMovement.cs
+++ JumpGame/Assets/Cuong/Assets/Character/PlayerMovement.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float dirX;
     // Start is called before the first frame update
     void Start()
     {
-        
+            rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetButton("Jump")){
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,7,0); 
+        dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 3f, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump")){
+           rb.velocity = new Vector2(rb.velocity.x,7f); 
+        }
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate(){
+        if(dirX > 0f){
+            anim.SetBool("Running", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }else if(dirX < 0f){
+            anim.SetBool("Running", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }else{
+            anim.SetBool("Running", false);
         }
     }
 }
